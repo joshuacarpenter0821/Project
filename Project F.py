23833b9166702f76d535fe4271da118f875e16a4
joshuacarpenter0821 <joshua.a.carpenter@du.edu>
2025-06-04 14:00:49 -0600

    Update Project F.py

--- Project F.py
+++ Project F.py
@@ -17,7 +17,7 @@ def get_cpi_data():
 # Load Starbucks financials
 @st.cache_data
 def load_financial_data():
-    df = pd.read_csv("starbucks_financials_expanded.csv", parse_dates=['Date'])
+    df = pd.read_csv("starbucks_financials_expanded.csv", parse_dates=['date'])
     df.set_index('Date', inplace=True)
     return df
 
